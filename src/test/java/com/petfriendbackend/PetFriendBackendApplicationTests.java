e96ae3d517eb82f9750ae8e8d9a55b298c6759c6
Petar Kiselinov <petarcross@hotmail.com>
2022-06-05 12:38:29 +0200

    Revert "Revert "added Pet class""
    This reverts commit 911a90184c03a4a86485ef830cdf069a550f271c.

---
+++ src/test/java/com/petfriendbackend/PetFriendBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petfriendbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetFriendBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
